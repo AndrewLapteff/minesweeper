7afdd49e2dc80bc393b720762042c84f9fc65505
Vladislavit <zvolinskiy.ukr.net@gmail.com>
2024-05-02 22:16:55 +0300

    Fixed piece_test.py

--- tests/piece_test.py
+++ tests/piece_test.py
@@ -72,35 +72,3 @@ def test_piece_get_has_bomb(piece: Piece):
     piece = Piece(True)
     assert piece.getHasBomb() == True
 
-
-import sys
-
-import pytest
-
-sys.path.append("src")
-from piece import Piece
-
-
-@pytest.fixture
-def piece():
-    return Piece(False)
-
-
-def test_piece_initialization(piece: Piece):
-    assert piece.getHasBomb() == False
-    assert piece.getClicked() == False
-    assert piece.getFlagged() == False
-    assert piece.getNumAround() == 0
-
-
-def test_piece_set_neighbors(piece: Piece):
-    neighbors = [Piece(False), Piece(True)]
-    piece.setNeighbors(neighbors)
-    assert piece.getNeighbors() == neighbors
-    assert piece.getNumAround() == 1
-
-
-def test_piece_get_neighbors(piece: Piece):
-    neighbors = [Piece(False), Piece(True)]
-    piece.setNeighbors(neighbors)
-    assert piece.getNeighbors() == neighbors
